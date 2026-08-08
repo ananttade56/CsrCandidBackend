6c7a66812cfc48f2a7e2ea40f3c7ac0cc7d867c7
ananttade56 <ananttade56@gmail.com>
2026-08-08 17:12:13 +0530

    again

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const clientUrls = process.env.CLIENT_URL
 const allowedOrigins = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://red-chimpanzee-237853.hostingersite.com/',
+    'https://csrcandid.in/',
     ...clientUrls
 ].filter(Boolean);
 
